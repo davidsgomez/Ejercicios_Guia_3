2bca9c745783a10e20667744f4c3486beda7ca67
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-09-06 20:16:51 -0600

    Add files via upload

---
+++ EjercicioN1/EjercicioN1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = { 97, 92, 81, 60 };
+
+        var query = from score in scores
+                    where score > 80
+                    select score;
+
+        foreach (var result in query)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ EjercicioN2/EjercicioN2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+        var consulta = from score in scores
+                       where score > 80
+                       orderby score descending
+                       select score;
+
+        Console.WriteLine("Resultados de la consulta:");
+
+        foreach (var score in consulta)
+        {
+            Console.WriteLine(score);
+        }
+    }
+}
